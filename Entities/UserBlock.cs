2d8d546a35e4e2873da248cf282413eae668fb52
hayrullah <hayrullah.cansu@gmail.com>
2018-09-03 14:56:36 +0300

    up

--- Entities/UserBlock.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OfflineMessaging.Entities
-{
-    public class UserBlock : TimeAwareEntity
-    {
-        public int Id { get; set; }
-        public string BlockerId { get; set; }
-        public string BlockedId { get; set; }
-    }
-}
